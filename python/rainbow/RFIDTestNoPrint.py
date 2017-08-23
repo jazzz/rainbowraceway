05eb062399e2ab37462be50ca166430abeb85032
Margravegit <enriqueventura@gmail.com>
2017-08-23 07:01:08 -0700

    Update RFIDTestNoPrint.py

--- python/rainbow/RFIDTestNoPrint.py
+++ python/rainbow/RFIDTestNoPrint.py
@@ -420,7 +420,7 @@ def showFunctions(): #Shows the terminal interface
 	return option
 
 def readValidCardList(): #Parses the list of registered cards from the file cardList.txt
-	f = open("/home/pi/rpi_ws281x/python/rainbow/cardList.txt")
+	f = open("/home/pi/rainbowraceway/python/rainbow/cardList.txt")
 	next = f.readline()
 	while next != "":
 		_card = next.split(':')
