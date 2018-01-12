0cfc05b573adcb00c4ab361a4bbbfe3500dbcd98
Jazz Turner-Baggs <jazz@jazzz.io>
2018-01-11 16:27:43 -0800

    increased pixel count

--- src/utils.py
+++ src/utils.py
@@ -105,7 +105,7 @@ else:
         def getPixelColor(self, n):
             return 0
 
-def create_and_init_neopixels(LED_COUNT=16, LED_PIN=18, LED_FREQ_HZ=800000, LED_DMA=5, LED_INVERT=False, LED_BRIGHTNESS=255, LED_CHANNEL=0,
+def create_and_init_neopixels(LED_COUNT=174, LED_PIN=18, LED_FREQ_HZ=800000, LED_DMA=5, LED_INVERT=False, LED_BRIGHTNESS=255, LED_CHANNEL=0,
                               LED_STRIP=ws.WS2811_STRIP_GRB):
 
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL,LED_STRIP)
